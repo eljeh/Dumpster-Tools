9e8f5a77f678a0b7ee6fd8880280144b3071583c
Ed <edbastarache@gmail.com>
2024-12-31 16:54:03 -0700

    Enhance API integration and environment variable usage across components
    - Updated multiple components to utilize environment variables for API authentication and bot ID, improving security and flexibility.
    - Refactored API fetch URLs in `GetBunkerList`, `GetPacks`, `GetPlayer`, and other components to dynamically use the bot ID, ensuring correct API interaction.
    - Added environment variable definitions in `astro.config.mjs` for better configuration management.
    - Cleaned up unused code and improved overall clarity in various components, enhancing maintainability.
    
    This update significantly improves the application's API interaction and security by leveraging environment variables.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,28 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
+  integrations: [tailwind(), vue()],
+  env: {
+    schema: {
+      SECRET_WBBOTID: envField.string({
+        context: "server",
+        access: "public"
+      }),
+      SECRET_WBAUTH: envField.string({
+        context: "server",
+        access: "public"
+      }),
+      PUBLIC_WBBOTID: envField.string({
+        context: "client",
+        access: "public"
+      }),
+      PUBLIC_WBAUTH: envField.string({
+        context: "client",
+        access: "public"
+      }),
+    }
+  }
 });
